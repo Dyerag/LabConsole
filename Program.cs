6d8e4be36d0318c5584063af6e9c68330af708de
dea <dea@PROELE78>
2023-11-30 12:42:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Channels;
+
+namespace LabConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int? input = null;
+            double radian;
+
+            do
+            {
+                Console.Write("Input the degress of the angle as a whole number: ");
+                if (!int.TryParse(Console.ReadLine(), out int intermediateStep))
+                {
+                    Console.WriteLine("you must input an angle, and with no decimals");
+                    Console.ReadKey(true);
+                    Console.Clear();
+                }
+                else
+                    input = intermediateStep;
+            } while (input == null);
+
+            radian = RadianCalc(input);
+            Console.WriteLine($"An angle of {input} Degrees results in a radian of {radian}");
+            Console.ReadKey(true);
+        }
+
+        public static double RadianCalc(int? input)
+        {
+            return Math.Round((int)input * Math.PI / 180,10);
+        }
+    }
+}
